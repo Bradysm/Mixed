339d82e41ca0d942ba1a8d0496fd849362c3a1c0
bradysm <bradysm@vt.edu>
2018-06-02 17:40:26 -0400

    Completed login screen

--- app/src/main/java/com/example/brady/beerig/MainActivity.java
+++ app/src/main/java/com/example/brady/beerig/MainActivity.java
@@ -45,9 +45,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_login);
 
-        keepNavBar();
+        //keepNavBar();
     }
 
     private void keepNavBar(){
