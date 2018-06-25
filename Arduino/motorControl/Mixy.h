b62794acea7822ff5ea163c26df24a922a651440
bradysm <bradysm@vt.edu>
2018-06-25 09:18:25 -0400

    completed multiple drinks and reformatted the code for reuse

--- Arduino/motorControl/Mixy.h
+++ Arduino/motorControl/Mixy.h
@@ -2,28 +2,18 @@
 #include <Adafruit_MotorShield.h>
 
 // full drink prototypes
-void vodkaCranberry();
-void screwdriver();
-void vodkaRedBull();
-void cokeAndRum();
-void tequilaSunrise();
+void vodkaCranberry(Adafruit_DCMotor *vodka, Adafruit_DCMotor *cran);
+void screwdriver(Adafruit_DCMotor *vodka, Adafruit_DCMotor *oj);
+void vodkaRedBull(Adafruit_DCMotor *vodka, Adafruit_DCMotor *redBull);
+void cokeAndRum(Adafruit_DCMotor *rum, Adafruit_DCMotor *coke);
+void tequilaSunrise(Adafruit_DCMotor *tequila, Adafruit_DCMotor *oj, Adafruit_DCMotor *grenadine);
+void ginAndTonic(Adafruit_DCMotor *gin, Adafruit_DCMotor *tonic);
+void vodkaTonic(Adafruit_DCMotor *vodka, Adafruit_DCMotor *tonic);
 
-// liquor prototypes
-void vodkaPour(Adafruit_DCMotor *motor, int time);
-void rumPour(Adafruit_DCMotor *motor, int time);
-void tequilaPour(Adafruit_DCMotor *motor, int time);
-void ginPour(Adafruit_DCMotor *motor, int time);
-
-// mixer prototypes
-void grapefruitPour(Adafruit_DCMotor *motor, int time);
-void ojPour(Adafruit_DCMotor *motor, int time); 
-void cranberryPour(Adafruit_DCMotor *motor, int time);
-void cokePour(Adafruit_DCMotor *motor, int time);
-void tonicPour(Adafruit_DCMotor *motor, int time);
-void grenadinePour(Adafruit_DCMotor *motor, int time);
-void redBullPour(Adafruit_DCMotor *motor, int time);
+// drink pour prototype
+void drinkPour(Adafruit_DCMotor *motor, long time);
 
 
 // constants
-const int ONE_OUNCE = 15000; // measured pouring one ounce of vodka (15 sec with tube disjunction)
-const int TWO_OUNCES = 30000; // (30 sec with tube disjuntion)
+const long ONE_OUNCE = 15000; // measured pouring one ounce of vodka (15 sec with tube disjunction)
+const long TWO_OUNCES = 30000; // (30 sec with tube disjuntion)
--- Arduino/motorControl/motorControl.ino
+++ Arduino/motorControl/motorControl.ino
@@ -14,12 +14,13 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *shield1Motor1 = AFMS.getMotor(1);
-Adafruit_DCMotor *shield1Motor2 = AFMS.getMotor(2);
+Adafruit_DCMotor *vodka = AFMS.getMotor(1); // M1 shield 1
+Adafruit_DCMotor *cranberry = AFMS.getMotor(2); // M2 shield 1
+Adafruit_DCMotor *orangeJuice = AFMS.getMotor(3); // M3 shield 
+Adafruit_DCMotor *tequila = AFMS.getMotor(4); // M4 shield 
 
-
-// boolean varaiables
-bool poured;
+// drink variable that is used to define drinks
+int drinkChoice;
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
@@ -27,16 +28,22 @@ void setup() {
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  shield1Motor1->setSpeed(250);
-  shield1Motor1->run(FORWARD);
-  shield1Motor2->setSpeed(250);
-  shield1Motor2->run(FORWARD);
+  vodka->setSpeed(250);
+  vodka->run(FORWARD);
+  cranberry->setSpeed(250);
+  cranberry->run(FORWARD);
+  orangeJuice->setSpeed(250);
+  orangeJuice->run(FORWARD);
+  tequila->setSpeed(250);
+  tequila->run(FORWARD);
   
   // turn on motor
-  shield1Motor1->run(RELEASE);
-  shield1Motor2->run(RELEASE);
+  vodka->run(RELEASE);
+  cranberry->run(RELEASE);
+  orangeJuice->run(RELEASE);
+  tequila->run(RELEASE);
 
-  poured = false;
+  drinkChoice = 0; // this will be the no-op value
 }
 
 /*
@@ -45,131 +52,160 @@ that will allow us to listen to bluetooth.
 */
 void loop() {
   // pour one vodka cran
-  if(!poured){
-    vodkaPour(shield1Motor1, ONE_OUNCE);
-    cranberryPour(shield1Motor2, TWO_OUNCES);
-    poured = true;
+  if(drinkChoice != 0){
+    // run through a switchcase list
+    switch(drinkChoice){
+      case 1:
+        vodkaCranberry(vodka, cranberry);
+        break;
+      case 2:
+        screwdriver(vodka, orangeJuice);
+        break;
+      case 3:
+        // vodkaRedBull(vodka, redBull);
+        break;
+      case 4:
+        // tequilaSunrise(tequila, orangeJuice, grenadine);
+        break;
+      case 5:
+        // ginAndTonic(gin, tonic);
+        break;
+      case 6:
+        // vodkaTonic(vodka, tonic);
+        break;
+      default: // catch any invalid number
+        break;
+    }
+    drinkChoice = 0; // reset the drink choice
   }
 
   // turn off the motors
-  shield1Motor1->run(RELEASE);
-  shield1Motor2->run(RELEASE);
+  vodka->run(RELEASE);
+  cranberry->run(RELEASE);
+  orangeJuice->run(RELEASE);
+  tequila->run(RELEASE);
   
   // wait for one second
   delay(1000);
 }
 
-/* this method will pour vodka for the 
- *  duration of the time given as the parameter
- *  @param time-time in miliseconds for vodka to pour
+/*
+ * This method will make one vodka cranberry 
+ * drink. This contains one ounce of vodka
+ * and 4.5 ounces of cranberry juice
+ * 
+ * @param-vodka: pointer to vodka motor
+ * @param-cran: pointer to cranberry juice motor
 */
-void vodkaPour(Adafruit_DCMotor *motor, int time){
+void vodkaCranberry(Adafruit_DCMotor *vodka, Adafruit_DCMotor *cran){
+  drinkPour(vodka, ONE_OUNCE);
+  drinkPour(cran, (long)(ONE_OUNCE * 4.5));
+}
+
+/**
+ * this method will make one screwdriver
+ * this contains 1.75 ounce of vodka 
+ * and 3.5 ounces of OJ
+ * should be served with ice
+ * 
+ * @param-vodka: pointer to vodka motor
+ * @param-oj: pointer to orange juice motor
+ */
+void screwdriver(Adafruit_DCMotor *vodka, Adafruit_DCMotor *oj){
+  drinkPour(vodka, (long)(ONE_OUNCE * 1.75));
+  drinkPour(oj, (long)(ONE_OUNCE * 3.5));
+}
+
+/**
+ * This method will make one vodka red bukll
+ * this contains 2 ounces of vodka
+ * and 4 ounces of redbull 
+ * should be served with ice
+ * 
+ * @param-vodka: pointer to vodka motor
+ * @param-redbull: pointer to redbull motor
+ */
+void vodkaRedBull(Adafruit_DCMotor *vodka, Adafruit_DCMotor *redBull){
+  drinkPour(vodka, TWO_OUNCES);
+  drinkPour(redBull, (long)(ONE_OUNCE * 4));
+}
+
+/**
+ * This method will make one coke and rum drink
+ * This drink contains four ounces of coke
+ * and 1 2/3 ounces of rum
+ * This drink should be served with lime
+ * 
+ * @param-vodka: pointer to vodka motor
+ * @param-redbull: pointer to redbull motor
+ */
+void cokeAndRum(Adafruit_DCMotor *rum, Adafruit_DCMotor *coke){
+  drinkPour(rum, (long)(ONE_OUNCE * 4));
+  drinkPour(coke, (long)(ONE_OUNCE * 1.667));
+}
+
+/**
+ * This method will make one tequila sunrise
+ * This drink contains 3 ounces of OJ
+ * 1 1/2 ounce of tequila and a half ounce
+ * of grenadine
+ * 
+ * @param-tequila: pointer to tequila motor
+ * @param-oj: pointer to OJ motor
+ * @param-grenadine: pointer to grenadine motor
+ * 
+  */
+void tequilaSunrise(Adafruit_DCMotor *tequila, Adafruit_DCMotor *oj, Adafruit_DCMotor *grenadine){
+  drinkPour(tequila, (long)(ONE_OUNCE * 1.5));
+  drinkPour(oj, (long)(ONE_OUNCE * 3));
+  drinkPour(grenadine, (long)(ONE_OUNCE * 0.5));
+}
+
+/**
+ * This method will make one gin and tonic
+ * This drink contains 3 ounces of gin
+ * 4 ounces of tonic
+ * This drink should be served with ice
+ * and lime juice
+ * 
+ * @param-gin: pointer to gin motor
+ * @param-tonic: pointer to tonic motor
+ * 
+  */
+void ginAndTonic(Adafruit_DCMotor *gin, Adafruit_DCMotor *tonic){
+  drinkPour(gin, (long)(ONE_OUNCE * 3));
+  drinkPour(tonic, (long)(ONE_OUNCE * 4));
+}
+
+/**
+ * This method will make one vodka-tonic
+ * This drink contains 2 ounces of vodka
+ * 4 ounces of tonic
+ * This drink should be served with ice
+ * and lime juice/wedge
+ * 
+ * @param-vodka: pointer to vodka motor
+ * @param-tonic: pointer to tonic motor
+ * 
+  */
+void vodkaTonic(Adafruit_DCMotor *vodka, Adafruit_DCMotor *tonic){
+  drinkPour(vodka, (long)(ONE_OUNCE * 2));
+  drinkPour(tonic, (long)(ONE_OUNCE * 4));
+}
+
+
+/*
+ * This method will be the main method used to
+ * pour any liquid. The user will define the motor
+ * for which to suck the liquid from and the duration
+ * for which it should do so
+ * 
+ * @param-motor: pointer pointing to specific Adafruit motor
+ * @param-time: time in milliseconds 
+*/
+void drinkPour(Adafruit_DCMotor *motor, long time){
   motor->run(FORWARD);
   delay(time);
   motor->run(RELEASE);
 }
-
-/* this will pour rum for the alotted
-  amount of time passed
-  @param time-time in miliseconds for pour
-*/
-void rumPour(Adafruit_DCMotor *motor, int time){
-  motor->run(FORWARD);
-  delay(time);
-  motor->run(RELEASE);
-}
-
-/* this will pour gin for the alotted
-  amount of time passed
-  @param time-time in miliseconds for pour
-*/
-void ginPour(Adafruit_DCMotor *motor, int time){
-  motor->run(FORWARD);
-  delay(time);
-  motor->run(RELEASE);
-}
-
-/* this will pour tequila for the alotted
-  amount of time passed
-  @param time-time in miliseconds for pour
-*/
-void tequilaPour(Adafruit_DCMotor *motor, int time){
-  motor->run(FORWARD);
-  delay(time);
-  motor->run(RELEASE);
-}
-
-
-/* this will pour cranberry juice for the alotted
-  amount of time passed
-  @param time-time in miliseconds for pour
-*/
-void cranberryPour(Adafruit_DCMotor *motor, int time){
-  motor->run(FORWARD);
-  delay(time);
-  motor->run(RELEASE);
-}
-
-/* this will pour grapefruit juice for the alotted
-  amount of time passed
-  @param time-time in miliseconds for pour
-*/
-void grapefruitPour(Adafruit_DCMotor *motor, int time){
-  motor->run(FORWARD);
-  delay(time);
-  motor->run(RELEASE);
-}
-
-/* this will pour orange juice for the alotted
-  amount of time passed
-  @param time-time in miliseconds for pour
-*/
-void ojPour(Adafruit_DCMotor *motor, int time){
-  motor->run(FORWARD);
-  delay(time);
-  motor->run(RELEASE);
-}
-
-/* this will pour coke for the alotted
-  amount of time passed
-  @param time-time in miliseconds for pour
-*/
-void cokePour(Adafruit_DCMotor *motor, int time){
-  motor->run(FORWARD);
-  delay(time);
-  motor->run(RELEASE);
-}
-
-/* this will pour tonic for the alotted
-  amount of time passed
-  @param time-time in miliseconds for pour
-*/
-void tonicPour(Adafruit_DCMotor *motor, int time){
-  motor->run(FORWARD);
-  delay(time);
-  motor->run(RELEASE);
-}
-
-/* this will pour grenadine for the alotted
-  amount of time passed
-  @param time-time in miliseconds for pour
-*/
-void grenadinePour(Adafruit_DCMotor *motor, int time){
-  motor->run(FORWARD);
-  delay(time);
-  motor->run(RELEASE);
-}
-
-/* this will pour redbull for the alotted
-  amount of time passed
-  @param time-time in miliseconds for pour
-*/
-void redBullPour(Adafruit_DCMotor *motor, int time){
-  motor->run(FORWARD);
-  delay(time);
-  motor->run(RELEASE);
-}
-
-
-
 
