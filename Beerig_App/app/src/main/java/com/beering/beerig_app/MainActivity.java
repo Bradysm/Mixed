43d614ec1870a9a9e37a7e70c614f0ec6048bf26
andrewhaus <andrewnhaus@gmail.com>
2018-07-02 00:43:31 -0400

    finished splash screen. Start partying button fades infinitely, but only fades in not out.

--- Beerig_App/app/src/main/java/com/beering/beerig_app/MainActivity.java
+++ Beerig_App/app/src/main/java/com/beering/beerig_app/MainActivity.java
@@ -3,10 +3,8 @@ package com.beering.beerig_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Display;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
 
 
@@ -17,24 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*final ImageView startPartying = findViewById(R.id.Start_Partying);
-        final Animation anim = AnimationUtils.loadAnimation(this, R.anim.fade);
-        startPartying.startAnimation(anim);
+        //Make Start Partying Fade in and out
+        ImageView startPartying = findViewById(R.id.Start_Partying);
+        AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        AlphaAnimation fadeOut = new AlphaAnimation(1.0f, 0.0f);
+        fadeIn.setRepeatCount(AlphaAnimation.INFINITE);
+        fadeOut.setRepeatCount(AlphaAnimation.INFINITE);
+        startPartying.startAnimation(fadeIn);
+        startPartying.startAnimation(fadeOut);
+        fadeIn.setDuration(1600);
+        fadeOut.setDuration(1600);
+        fadeOut.setStartOffset(fadeIn.getStartOffset());
+        fadeIn.setStartOffset(fadeOut.getStartOffset());
 
-        new java.util.Timer().schedule(
-                new java.util.TimerTask() {
-                    @Override
-                    public void run() {
-                        MainActivity.this.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                startPartying.startAnimation(anim);
-                            }
-                        });
-                    }
-                },
-                4000
-        );*/
+
+
     }
 
     public void startHomeActivity(View view) {
