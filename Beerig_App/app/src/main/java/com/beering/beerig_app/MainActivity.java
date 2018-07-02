bdd89237fa5c3b00580f3ffffbd5031715a9afc9
andrewhaus <andrewnhaus@gmail.com>
2018-07-01 23:55:38 -0400

    created home screen activity

--- Beerig_App/app/src/main/java/com/beering/beerig_app/MainActivity.java
+++ Beerig_App/app/src/main/java/com/beering/beerig_app/MainActivity.java
@@ -1,7 +1,14 @@
 package com.beering.beerig_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*final ImageView startPartying = findViewById(R.id.Start_Partying);
+        final Animation anim = AnimationUtils.loadAnimation(this, R.anim.fade);
+        startPartying.startAnimation(anim);
+
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                startPartying.startAnimation(anim);
+                            }
+                        });
+                    }
+                },
+                4000
+        );*/
+    }
+
+    public void startHomeActivity(View view) {
+        Intent intent = new Intent(this, HomeScreen.class);
+        MainActivity.this.startActivity(intent);
     }
 }
