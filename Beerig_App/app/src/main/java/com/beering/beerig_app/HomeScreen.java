2d36c220c243be253cfe3486127422e935c035f4
andrewhaus <andrewnhaus@gmail.com>
2018-07-02 09:29:59 -0400

    Added activities for tequila, rum, gin, and vodka. Added buttons that take you to each activity in HomeScreen.java

--- Beerig_App/app/src/main/java/com/beering/beerig_app/HomeScreen.java
+++ Beerig_App/app/src/main/java/com/beering/beerig_app/HomeScreen.java
@@ -1,7 +1,9 @@
 package com.beering.beerig_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+    public void startTequilaActivity(View view){
+        Intent tequilaIntent = new Intent(this, TequilaActivity.class);
+        HomeScreen.this.startActivity(tequilaIntent);
+    }
+
+    public void startRumActivity(View view){
+        Intent rumActivity = new Intent(this, RumActivity.class);
+        HomeScreen.this.startActivity(rumActivity);
+    }
+
+    public void startGinActivity(View view){
+        Intent ginActivity = new Intent(this, GinActivity.class);
+        HomeScreen.this.startActivity(ginActivity);
+    }
+
+    public void startVodkaActivity(View view){
+        Intent vodkaActivity = new Intent(this, VodkaActivity.class);
+        HomeScreen.this.startActivity(vodkaActivity);
     }
 }
