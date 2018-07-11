e36c8deb4c34b7fba36a74d2f988811d2ea7ba99
bradysm <bradysm@vt.edu>
2018-07-11 00:45:56 -0400

    added a scrolling view to the rum section

--- Beerig_App/app/src/main/java/com/beering/beerig_app/PartyModeActivity.java
+++ Beerig_App/app/src/main/java/com/beering/beerig_app/PartyModeActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class PartyModeActivity extends AppCompatActivity {
+    private ImageView partyDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_party_mode);
-
+        partyDescription = (ImageView) findViewById(R.id.party_mode_description);
+        partyDescription.bringToFront();
+        partyDescription.invalidate();
 
     }
 }
