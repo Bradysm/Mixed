c3331d0db6389a6c7bce7263c45fe67e27ca93ba
andrewhaus <andrewnhaus@gmail.com>
2018-07-11 20:06:35 -0400

    Added more to Dialog

--- Beerig_App/app/src/main/java/com/beering/beerig_app/ConfirmDrinkFragment.java
+++ Beerig_App/app/src/main/java/com/beering/beerig_app/ConfirmDrinkFragment.java
@@ -3,6 +3,7 @@ package com.beering.beerig_app;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -17,8 +18,27 @@ public class ConfirmDrinkFragment extends Fragment {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+        //Temporary Dialog Title
+        builder.setMessage(R.string.Start_Partying)
+                .setTitle(R.string.app_name);
 
 
+        //Dialog's Confirm Button
+        builder.setPositiveButton(R.string.pour, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //User Clicks Pour Drink Button
+            }
+        });
+        //Dialog's Cancel Button
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //User Clicks Cancel Button
+            }
+        });
+
+        
         return builder.create();
     }
 
